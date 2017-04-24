f995b55738034823edefdd617c2bde714608e2d0
libin <825783131@qq.com>
2017-04-24 11:07:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/mengcengdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.administrator.mengcengdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GuideView guideView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void initView() {
+        TextView textView = (TextView) findViewById(R.id.tv);
+        // 使用图片
+        final ImageView iv = new ImageView(this);
+        iv.setImageResource(R.drawable.img_new_task_guide);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        iv.setLayoutParams(params);
+        guideView = GuideView.Builder
+                .newInstance(this)
+                .setTargetView(textView)//设置目标
+                .setCustomGuideView(iv)
+                .setDirction(GuideView.Direction.BOTTOM)
+                .setShape(GuideView.MyShape.CIRCULAR)   // 设置圆形显示区域，
+                .setBgColor(getResources().getColor(R.color.shadow))
+                .setOnclickListener(new GuideView.OnClickCallback() {
+                    @Override
+                    public void onClickedGuideView() {
+                        guideView.hide();
+                    }
+                }).build();
+        guideView.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initView();
+    }
+}
