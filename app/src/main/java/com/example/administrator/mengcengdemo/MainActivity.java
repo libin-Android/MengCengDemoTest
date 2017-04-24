3931f560e22876616e89c8937a9be68abbf37a9f
libin <825783131@qq.com>
2017-04-24 15:54:44 +0800

    朦层修改

--- app/src/main/java/com/example/administrator/mengcengdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/mengcengdemo/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        //测试
+        //测试aaaaaaaaaaaa
         initView();
     }
 }
